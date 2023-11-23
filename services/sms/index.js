89fc0340450335675cdf099e2fbd9c55d0a98036
Nguyenkhanh <nhkhanh1998@gmail.com>
2023-11-23 23:07:13 +0700

    implement send sms

--- services/sms/index.js
+++ services/sms/index.js
@@ -1,7 +1,5 @@
-const accountSid =
-  process.env.TWILIO_ACCOUNT_SID || "SK697dff8d8a77c5d27214f8c9b92bad33";
-const authToken =
-  process.env.TWILIO_AUTH_TOKEN || "JEf9sMTNthcmJNXnBfb7ZZtTZmYZk7DR";
+const accountSid = process.env.TWILIO_ACCOUNT_SID || "";
+const authToken = process.env.TWILIO_AUTH_TOKEN || "";
 const client = require("twilio")(accountSid, authToken);
 
 export const sendSMS = async ({ from = "+18643324366", to, body }) => {
